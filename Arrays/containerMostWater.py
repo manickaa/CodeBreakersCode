f4e663a93790d04f6eaf2df47b14dd2e12aebfee
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-06-11 10:57:40 -0700

    Two pointer approach to get most water area

--- Arrays/containerMostWater.py
+++ Arrays/containerMostWater.py
@@ -1,6 +1,10 @@
 #      p1              p2
 # arr [1,8,6,2,5,4,8,3,7]
 def containerMostWater(arr):
+
+    #O(N) runtime complexity since both the pointers cover half of the array each.
+    #O(1) space complexity since no extra space used
+
     if len(arr) < 2:
         return -1
     p1 = 0
@@ -19,3 +23,7 @@ def containerMostWater(arr):
 
 arr = [1,8,6,2,5,4,8,3,7]
 print(containerMostWater(arr)) #49
+arr = [1]
+print(containerMostWater(arr)) #-1
+arr = [9,1,2,3,6,7,8,9]
+print(containerMostWater(arr)) #63
