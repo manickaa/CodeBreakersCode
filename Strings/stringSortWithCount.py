fe43d1291a855d0cd3109ee30f50afd0e42be1f6
Aishwarya Manicka <manickaa@oregonstate.edu>
2021-07-08 17:46:19 -0700

    sort string with counts

--- Strings/stringSortWithCount.py
+++ Strings/stringSortWithCount.py
@@ -1,12 +1,14 @@
 def stringSort(string):
+    #O(A*N) time => O(N) time
+    #space => O(N) for result. Constant extra space
     alphabets = 26
     counts = [0] * alphabets
 
-    for char in string.replace(" ", ""):
+    for char in string.replace(" ", ""): #runs N times
         counts[ord(char) - ord('a')] += 1
 
     sorted_string = ""
-    for i, count in enumerate(counts):
+    for i, count in enumerate(counts): #runs A times in worst case
         sorted_string += chr(ord('a') + i) * count
     
     return sorted_string
